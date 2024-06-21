5cc1bbfce1ef0769b83a140628296a659b080968
kenway214 <kenway214@outlook.com>
2024-06-21 16:03:04 +0530

    Cheetah: Include vendorsetup.sh
    Signed-off-by: kenway214 <kenway214@outlook.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+echo 'Starting to clone stuffs needed to build for cheetah'
+
+# Device common
+echo 'Cloning common device tree'
+
+git clone https://github.com/Pixel7Pro-Cheetah/device_google_gs201.git -b lineage-21 device/google/gs201
+git clone https://github.com/Pixel7Pro-Cheetah/device_google_gs101.git -b lineage-21 device/google/gs101
+git clone https://github.com/LineageOS/android_device_google_gs-common -b lineage-21 device/google/gs-common
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone --depth=1 https://android.googlesource.com/device/google/pantah-kernel device/google/pantah-kernel
+# Vendor
+echo 'Cloning vendor tree'
+git clone https://github.com/Pixel7Pro-Cheetah/proprietary_vendor_google_cheetah.git -b lineage-21 vendor/google/cheetah
