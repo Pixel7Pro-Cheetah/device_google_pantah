aeecc1cff2f508aba941551e8a073e995dc56b8f
kenway214 <kenway214@outlook.com>
2024-07-20 00:18:19 +0530

    cheetah: everest-ify

--- everest_cheetah.mk
+++ everest_cheetah.mk
@@ -4,9 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common ProjectEverest stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
@@ -17,7 +17,16 @@ include device/google/pantah/cheetah/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7 Pro
-PRODUCT_NAME := lineage_cheetah
+PRODUCT_NAME := everest_cheetah
+
+# Everest
+WITH_GAPPS := true
+EVEREST_BUILD_TYPE := UNOFFICIAL
+EVEREST_MAINTAINER := Kenway241
+TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
